e5cdd7b0432f35802c45c9014ed666be140d3cf5
d0bee <gusdud9745@gmail.com>
2024-01-02 21:28:38 +0900

    240102 section 11

--- RPG_Unity/Assets/Scripts/Managers/DataManager.cs
+++ RPG_Unity/Assets/Scripts/Managers/DataManager.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 참조 인터페이스
+public interface ILoader<Key, Value>
+{
+    Dictionary<Key, Value> MakeDict();
+}
+
 public class DataManager 
 {
+    // 관리하기 용이하도록 int level, Stat 형태의 Dictionary 생성
+    public Dictionary<int, Stat> StatDict {  get; private set; } = new Dictionary<int, Stat>();
     public void init()
     {
-        
+        StatDict = LoadJson<StatData, int, Stat>("StatData").MakeDict();
+    }
+
+    Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
+    {
+        // local에서 json 파일 Load
+        TextAsset testAsset = Managers.Resource.Load<TextAsset>($"Data/{path}");
+        // from Load한 json 파일 -> testAsset.text형식 반환
+        return JsonUtility.FromJson<Loader>(testAsset.text);
     }
 }
